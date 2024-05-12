cce7a3a8b60eba99b0d11e6bc3dbe8d8426574d4
Jean-Marie Alder <jeanmarie.alder@gmail.com>
2024-05-12 20:56:06 +0200

    clean structure for full process

---
+++ main.py
@@ -0,0 +1,40 @@
+
+
+def main():
+    """
+    Main function to orchestrate the entire process.
+
+    Steps:
+    1. Data preprocessing steps
+    2. Graph generation
+    3. Graph persistence (saving graph to a neo4j database)
+    4. Apply community detection
+    5. Analyze results
+    """
+    
+    print("Starting process...")
+
+    # 1. Data preprocessing steps
+    print()
+    print("Data Preprocessing")
+
+    # 2. Graph generation
+    print()
+    print("Graph Generation")
+
+    # 3. Graph persistence
+    print()
+    print("Graph persistence")
+    # TODO: save graph to a neo4j database.
+
+    # 4. Apply community detection
+    print()
+    print("Community Detection")
+
+    # 5. Analyse results
+    print()
+    print("Analyse results")
+
+
+if __name__ == "__main__":
+    main()
